f27d4ff8dc9389ceabacc8900e1610218a6b69ef
xanderwilson030 <54119525+xanderwilson030@users.noreply.github.com>
2023-09-21 14:40:09 -0400

    Resolved Script Overwrite

--- RockinRacket/Assets/Scripts/MiniGames/TShirtCannon.cs
+++ RockinRacket/Assets/Scripts/MiniGames/TShirtCannon.cs
@@ -24,18 +24,19 @@ public class TShirtCannon : MiniGame, IPointerDownHandler
 {
     // Public Variables
     [Header("Object References")]
-    [SerializeField] Sprite cannon;
+    // May not longer be needed... requires further investigation
     [SerializeField] GameObject shirt;
+    // Used for Debugging
     [SerializeField] Camera cameraMain;
 
     [Header("Cannon Pressure Bar")]
     [SerializeField] CannonBar cannonBar;
+    [Tooltip("MUST be a value that is divisible by THREE")]
     [SerializeField] int maxPressure;
-    [SerializeField] int pressureIncrement;
+    [SerializeField] int pressureIncrementMulitplier;
 
     [Header("Shot Counter")]
     [SerializeField] TMP_Text shotCounterText;
-
 
     [Header("Variables")]
     [SerializeField] int cannonRange;
@@ -48,17 +49,19 @@ public class TShirtCannon : MiniGame, IPointerDownHandler
     private int remainingShots;
     private float localPressureValue = 0;
     private float currentFame = 0;
-    private bool fireShirt = false;
     private Camera currentCamera;
-    private Transform start;
-    private Transform end;
     private bool isEventOpen = false;
+    private bool cyclePressureBar = false;
 
     // Debug Variables
     bool[] successfulShots;
 
-    public override void Activate() 
+    /*
+     * Activate runs when the event is spawn in, NOT when the event is opened by the player
+     */
+    public override void Activate()
     {
+        // Code from the Activate method in the Minigame.cs class
         isActiveEvent = true;
         remainingDuration = duration;
         if (!infiniteDuration)
@@ -67,111 +70,164 @@ public class TShirtCannon : MiniGame, IPointerDownHandler
             durationCoroutine = StartCoroutine(EventDurationCountdown());
         }
         GameEvents.EventStart(this);
-        
-        // Setting our variables for the cannon pressure bar
+
+        // The following code is custom for this minigame...
+
+        // We need to set the cannon bar's max pressure and value, but not start it until the player opens the event
         cannonBar.SetMaxValue(maxPressure);
         cannonBar.SetValue(0);
-        //CycleCannonBar();
 
-        // Setting the number of remaining t-shirts left to be shot and our text object
+        // Setting the T-Shirt ammo counter and initializing the UI element
         remainingShots = 0;
         shotCounterText.text = "Remaining Shirts: " + (maxNumShots - remainingShots);
 
         // Setting the value for our debug array
         successfulShots = new bool[maxNumShots];
-        
-        Debug.Log("Activated");
+
+        // Debug Statement
+        Debug.Log("<color=green>T-Shirt Minigame Activated and Spawned In</color>");
     }
 
+    /*
+     * OpenEvent runs when the player opens the minigame
+     */
     public override void OpenEvent()
     {
-        isEventOpen = true;
-        GameEvents.EventOpened(this); 
+        // The following code is from the parent Minigame.cs class
+        GameEvents.EventOpened(this);
         HandleOpening();
-        Debug.Log("Event Opened");
-        // Calling the Cinemachine camera switcher
-        
+
+        // The following code is custom for this minigame...
+        isEventOpen = true; // This variable is used to block the event's factors to trigger early
+
+        // Calling our Cinemachine game event to swap the camera using the Cinemachine animator
         CinemachineGameEvents.instance.e_SwitchToTShirtCam.Invoke();
 
-        // Grabbing the current camera for object reference
+        // Setting the boolean that allows us to the cannon pressure bar
+        cyclePressureBar = true;
+
+        // Grabbing the current camera to use with Raycasting as a part of this minigame
         currentCamera = Camera.main;
         cameraMain = currentCamera;
-        start = currentCamera.transform;
-        
+
+        // Debug Statement
+        Debug.Log("<color=aqua>Player has opened T-Shirt Minigame Event</color> | OpenEvent() has been run");
     }
 
-    // End/Complete
+    /*
+     * CloseEvent() runs when the event is closed by the player
+     */
     public override void CloseEvent()
     {
-        isEventOpen = false;
-        GameEvents.EventClosed(this); 
+        // The following code is from the parent Minigame.cs class
+        GameEvents.EventClosed(this);
         HandleClosing();
+
+        // The following code is custom for this minigame
+        isEventOpen = false; // Setting our gatekeeper variable to false to prevent false runs
+        // Switching the camera back to the band-facing one
         CinemachineGameEvents.instance.e_SwitchToBandCam.Invoke();
+        // Stopping the pressure bar from cycling
+        cyclePressureBar = false;
     }
 
-    // Miss
+    /*
+     * EndEvent() runs when the minigame is forceably closed by the game or the event is over
+     */
     public override void End()
     {
         base.End();
+
+        // Switching the camera back to the band-facing one
         CinemachineGameEvents.instance.e_SwitchToBandCam.Invoke();
+
+        // Stopping the pressure bar from cycling
+        cyclePressureBar = false;
     }
 
+    /*
+     * HandleClosing handles the fine details of closing the event, and is called by CloseEvent()
+     */
     public override void HandleClosing()
     {
         Panels.SetActive(false);
+
+        // Swapping the camera back to the band-facing one
         CinemachineGameEvents.instance.e_SwitchToBandCam.Invoke();
 
         //If you want to reset the game if they did not complete it
         if (IsCompleted == false)
         { RestartMiniGameLogic(); }
+
+        // Stopping the pressure bar from cycling
+        cyclePressureBar = false;
     }
 
     private void Update()
     {
-        //// This method is called only when a shirt firing animation needs played
-        //if (fireShirt)
-        //{
-        //    fireShirt = false;
-        //    FireShirtAnimationHelper();
-        //}
+        // The cannon pressure bar is only cycled when this 'gatekeeping' boolean is triggered
+        if (cyclePressureBar)
+        {
+            //CycleCannonBar();
+
+            if (localPressureValue < maxPressure)
+            {
+                localPressureValue += (Time.deltaTime * pressureIncrementMulitplier);
+            }
+            else if (localPressureValue > 0)
+            {
+                localPressureValue -= (Time.deltaTime * pressureIncrementMulitplier);
+            }
+        }
     }
 
     /*
      * This method cycles the pressure bar back and forth
+     * 
+     * ... Possibly going to be depreciated
      */
     private void CycleCannonBar()
     {
-        while(IsCompleted == false)
-        {
-            if (localPressureValue < maxPressure)
-            {
-                localPressureValue += pressureIncrement;
-            }
-            else if (localPressureValue > 0)
-            {
-                localPressureValue -= pressureIncrement;
-            }
-        }
+        //while(IsCompleted == false)
+        //{
+        //    if (localPressureValue < maxPressure)
+        //    {
+        //        localPressureValue += pressureIncrement;
+        //    }
+        //    else if (localPressureValue > 0)
+        //    {
+        //        localPressureValue -= pressureIncrement;
+        //    }
+        //}
     }
 
     /*
-     * This method takes player input when they click the mouse and determines where the t-shirt
-     * lands and at what pressure amount it was fired at
+     * This method takes player input when they click the mouse and determines what the 
+     * player is hitting and at what pressure amount
      */
     public void OnPointerDown(PointerEventData eventData)
     {
+        // These to three statements are used to safeguard the raycasting from any NULL errors
+        if (!isActiveEvent)
+        {
+            Debug.Log("<color=red>Event not active</color>");
+            return;
+        }
 
-        if (!isActiveEvent) {return;}
-
-        if (isEventOpen) { return; }
+        //if (isEventOpen) {
+        //    Debug.Log("<color=red>Event not open</color>");
+        //    return; 
+        //}
 
         if (currentCamera == null)
         {
+            Debug.Log("<color=red>Current Camera is NULL</color>");
             currentCamera = Camera.main;
             cameraMain = currentCamera;
         }
 
-
+        // This block of code evaluates the current pressure level of the cannon bar
+        // Right now the pressure value CAN ONLY BE a value divisible by THREE
         string currentPressure = "";
 
         if (localPressureValue < maxPressure / 3)
@@ -190,24 +246,34 @@ public class TShirtCannon : MiniGame, IPointerDownHandler
             currentPressure = "Good";
         }
 
-        
+
 
         // Cast a ray from the camera's position through the mouse's position on the screen
         Ray ray = currentCamera.ScreenPointToRay(eventData.position);
-        RaycastHit hit; 
+        RaycastHit hit;
+
+        // TODO - Fix raycast bug
 
         if (Physics.Raycast(ray, out hit, cannonRange))
         {
             GameObject target = hit.transform.gameObject;
 
-            if (target.CompareTag("Audience")) 
+            // Debug Statement
+            Debug.Log("<color=orange>Raycast is fired</color>");
+            Debug.DrawRay(cameraMain.transform.position, cameraMain.transform.TransformDirection(Vector3.forward) * hit.distance, Color.red, 1000);
+            Debug.Log("Did Hit" + hit.transform.gameObject.tag);
+
+            // TODO - Play a sound when the shirt is fired
+
+            // Detecting what the player hits
+            if (target.CompareTag("Audience"))
             {
-                Debug.Log("Shot lands successfully");
+                Debug.Log("<color=green>Shot lands successfully</green>");
                 FireShirt(true, currentPressure, target);
             }
             else
             {
-                Debug.Log("Shot misses...");
+                Debug.Log("<color=orange>Shot misses...</color>");
                 FireShirt(false, currentPressure, target);
             }
         }
@@ -219,12 +285,8 @@ public class TShirtCannon : MiniGame, IPointerDownHandler
      */
     private void FireShirt(bool hitsAudience, string pressure, GameObject target)
     {
-        // Setting off the animation and assigning a value to our target
-        end = target.transform;
-        fireShirt = true;
-
         // Setting the value for our debug array and then incrementing the remainingShots variable
-        successfulShots[remainingShots] = hitsAudience;
+        //successfulShots[remainingShots] = hitsAudience;
         remainingShots++;
         shotCounterText.text = "Remaining Shirts: " + (maxNumShots - remainingShots);
         // If the player is out, the game ends and we send the appropriate variables back to the game manager
@@ -238,15 +300,38 @@ public class TShirtCannon : MiniGame, IPointerDownHandler
             End();
         }
 
+        // Triggering our method in the Audience class
+        if (hitsAudience)
+        {
+            target.GetComponent<Audience>().PlayTCMReaction(pressure);
+        }
+
         if (hitsAudience && pressure.Equals("Good"))
         {
-            currentFame += fameIncrementer * fameComboMultiplier;
+            /*
+             * TODO - Get audience class from target to trigger T-Shirt animation
+             */
+            //currentFame += fameIncrementer * fameComboMultiplier;
+        }
+        else if (hitsAudience && pressure.Equals("Weak"))
+        {
+            // TODO - Call audience class and play animation once created
+            //currentFame -= fameDecrementer * fameComboMultiplier;
+        }
+        else if (hitsAudience && pressure.Equals("Bad"))
+        {
+            // TODO - Call audience class and play animation once created
+            //currentFame -= fameDecrementer * fameComboMultiplier;
+
         }
         else
         {
-            // Negative player feedback here?
+            // This case handles when the player misses the audience and hits something else
+
+            // TODO - Implement T-Shirt missed animation when it is created
+
+            currentFame += fameDecrementer * fameComboMultiplier;
+
         }
     }
-
-    
 }
