cd12c815d17684dad6f5b53703455b5fd869df0b
Hamilton Gilbert <48567605+HamiltonGilbert@users.noreply.github.com>
2023-10-30 18:40:29 -0400

    pause menu scaling couroutine added

---
+++ RockinRacket/Assets/Scripts/Pause Menu/IpodOption.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IpodOption : MonoBehaviour
+{
+    public IEnumerator ScaleOverTime(Vector3 toScale, Quaternion toRotation, float duration)
+    {
+        float counter = 0;
+
+        //Get the current scale of the object to be moved
+        Vector3 startScaleSize = transform.localScale;
+
+        while (counter < duration)
+        {
+            counter += Time.deltaTime;
+            transform.localScale = Vector3.Lerp(startScaleSize, toScale, counter / duration);
+            //transform.rotation = 
+            yield return null;
+        }
+    }
+}
---
+++ RockinRacket/Assets/Scripts/Pause Menu/PauseMenuNavigation.cs
@@ -0,0 +1,80 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenuNavigation : MonoBehaviour
+{
+    [SerializeField] private GameLoadHandler gameLoadHandler;
+    [SerializeField] private GameObject[] ipodOptions;
+    private int index = -1;
+    private float scalingDuration = .1f;
+
+    public void Up() {
+        if (index > 0)
+        {
+            index--;
+            UpdateOptions(index + 1, index);
+        }
+        if (index == -1)
+        {
+            index = ipodOptions.Length;
+            UpdateOptions(index - 1, index);
+        }
+    }
+    public void Down() {
+        if (index < ipodOptions.Length - 1)
+        {
+            index++;
+            UpdateOptions(index - 1, index);
+        }
+        if (index == -1)
+        {
+            index++;
+            UpdateOptions(index - 1, index);
+        }
+    }
+
+    public void Select()
+    {
+        if (index == 0)
+            gameLoadHandler.OpenMenu();
+        else if (index == 1)
+            gameLoadHandler.OpenSettings();
+        else if (index == 2)
+        {
+            gameLoadHandler.SaveAndExit();
+        }
+    }
+
+    public void Reset()
+    {
+        ipodOptions[index].transform.localScale = new Vector3(1f, 1f, 1f);
+        index = -1;
+    }
+
+    private void UpdateOptions(int oldOption, int newOption)
+    {
+        if (newOption >= 0 && newOption < ipodOptions.Length)
+        {
+            StartCoroutine(ScaleOverTime(ipodOptions[newOption].transform, new Vector3(1.2f, 1.2f, 1f), scalingDuration));
+            if (oldOption >= 0 && oldOption < ipodOptions.Length)
+                StartCoroutine(ScaleOverTime(ipodOptions[oldOption].transform, new Vector3(1f, 1f, 1f), scalingDuration)); ;
+        }
+    }
+
+    public IEnumerator ScaleOverTime(Transform objectTransform, Vector3 toScale, float duration)
+    {
+        float counter = 0;
+
+        //Get the current scale of the object to be moved
+        Vector3 startScaleSize = objectTransform.localScale;
+
+        while (counter < duration)
+        {
+            counter += Time.deltaTime;
+            objectTransform.localScale = Vector3.Lerp(startScaleSize, toScale, counter / duration);
+            //transform.rotation = 
+            yield return null;
+        }
+    }
+}
