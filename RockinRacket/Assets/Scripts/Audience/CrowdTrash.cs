3e4610ec7d13962cd80bd151200413b861df17c4
Stealthinator <kevschoo@iu.edu>
2023-11-25 04:47:28 -0500

    Updated Trash to fix error in builds

--- RockinRacket/Assets/Scripts/Audience/CrowdTrash.cs
+++ RockinRacket/Assets/Scripts/Audience/CrowdTrash.cs
@@ -7,9 +7,11 @@ public class CrowdTrash : MonoBehaviour
 {
     public bool IsProjectile { get; set; } = true;
     private bool isDragging = false;
-    private Vector2 lastMousePosition;
+    private Vector2 lastPosition;
+    private float lastTime;
     private Rigidbody2D rb;
     public float maxVelocity = 10f; 
+    public float updateInterval = 0.1f; 
 
     void Start()
     {
@@ -22,24 +24,33 @@ public class CrowdTrash : MonoBehaviour
         {
             Vector2 mousePosition = Camera.main.ScreenToWorldPoint(Mouse.current.position.ReadValue());
             transform.position = mousePosition;
-            Vector2 velocity = (mousePosition - lastMousePosition) / Time.deltaTime;
-            velocity = Vector2.ClampMagnitude(velocity, maxVelocity);
-            rb.velocity = velocity;
 
-            lastMousePosition = mousePosition; 
+            if (Time.time - lastTime >= updateInterval)
+            {
+                lastPosition = mousePosition;
+                lastTime = Time.time;
+            }
         }
     }
 
     public void StartDragging()
     {
         isDragging = true;
-        rb.gravityScale = 0; 
-        lastMousePosition = Camera.main.ScreenToWorldPoint(Mouse.current.position.ReadValue());
+        rb.gravityScale = 0;
+        lastPosition = Camera.main.ScreenToWorldPoint(Mouse.current.position.ReadValue());
+        lastTime = Time.time;
     }
 
     public void StopDragging()
     {
+        Vector2 endPosition = Camera.main.ScreenToWorldPoint(Mouse.current.position.ReadValue());
+        float endTime = Time.time;
+
+        Vector2 velocity = (endPosition - lastPosition) / (endTime - lastTime);
+        velocity = Vector2.ClampMagnitude(velocity, maxVelocity);
+        rb.velocity = velocity;
+
         isDragging = false;
-        rb.gravityScale = 1; 
+        rb.gravityScale = 1;
     }
 }
