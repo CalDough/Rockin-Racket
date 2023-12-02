00ab922bcd9b8146fd821536fb9178816dad0f96
Hamilton Gilbert <48567605+HamiltonGilbert@users.noreply.github.com>
2023-12-02 12:50:40 -0500

    new hub has interactable board

---
+++ RockinRacket/Assets/Scripts/MainMenu_Hub/HubManager.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HubManager : MonoBehaviour
+{
+    [SerializeField] private GameObject boardScreen;
+
+    public void LookAtBoard()
+    {
+        boardScreen.SetActive(true);
+    }
+
+    private void Start()
+    {
+        boardScreen.SetActive(false);
+    }
+}
