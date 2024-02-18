9958b77ac9c0861ec876477bed58227c7cada3fb
Hamilton Gilbert <48567605+HamiltonGilbert@users.noreply.github.com>
2024-02-18 15:06:04 -0500

    updated SceneIndex to remove MainMenu

---
+++ RockinRacket/Assets/SaveSystem (Hamilton)/SceneIndex.cs
@@ -0,0 +1,31 @@
+public enum SceneIndex
+{
+    StartMenu, // 0
+    Loading, // 1
+    VenueSelection, // 2
+    BandManagement, // 3
+    Shop, // 4
+    UpgradeManagement, // 5
+    SongSelection, // 6 TODO remove
+    SongSelectionAfterVenue, // 7 TODO remove
+    ConcertDefault, // 8
+    SettingsMenu, // 9
+    Cinematic1, // 10
+    Cinematic2, // 11
+    BeforeFirstConcert, // 12
+    Hub, // 13
+    LevelOne, //14
+    Intermission, //15
+    LevelTwo, //16
+    LevelThree, //17
+    LevelFour, //18
+    Cinematic3, //19
+    Cinematic4, //20
+    Cinematic5, //21
+    Cinematic6, //22
+    Cinematic7, //23
+    Cinematic8, //24
+    Cinematic9, //25
+    ConcertTutorial, //26
+    IntermissionTutorial //27
+};
