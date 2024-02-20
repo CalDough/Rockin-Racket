9dd13d59750a7f9aa7a627fe0129bbb801aa4ad5
Hamilton Gilbert <48567605+HamiltonGilbert@users.noreply.github.com>
2024-02-19 21:47:28 -0500

    added interactivity to level start screens

--- RockinRacket/Assets/Scripts/UserInterface/TipPaper.cs
+++ RockinRacket/Assets/Scripts/UserInterface/TipPaper.cs
@@ -1,15 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
-using UnityEngine.EventSystems;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
-[System.Serializable]
-public class Sticker : MonoBehaviour, IPointerDownHandler, IPointerEnterHandler, IPointerUpHandler
+public class TipPaper : MonoBehaviour, IPointerDownHandler, IPointerEnterHandler, IPointerUpHandler
 {
-    public Bandmate bandmate;
     private bool draggable = false;
     private Vector3 mouseDiff;
-
+    
     private void Update()
     {
         if (draggable)
@@ -25,18 +23,13 @@ public class Sticker : MonoBehaviour, IPointerDownHandler, IPointerEnterHandler,
     public void OnPointerDown(PointerEventData eventData)
     {
         transform.SetAsLastSibling();
-        transform.localScale = new Vector3(1.2f, 1.2f, 1f);
+        transform.localScale = new Vector3(1.02f, 1.02f, 1f);
         mouseDiff = transform.position - Input.mousePosition;
         draggable = true;
     }
     public void OnPointerUp(PointerEventData eventData)
     {
-        transform.localScale = new Vector3(1.1f, 1.1f, 1f);
+        transform.localScale = new Vector3(1f, 1f, 1f);
         draggable = false;
-        StickerSaver.UpdateSticker(bandmate, transform);
-    }
-    public void SetDraggable()
-    {
-        draggable = true;
     }
 }
