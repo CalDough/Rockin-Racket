be3f4f6f1e17c81ad9bd15e48db45a8afb3e38b1
Hamilton Gilbert <48567605+HamiltonGilbert@users.noreply.github.com>
2024-02-25 14:32:02 -0500

    saved dialogue data

--- RockinRacket/Assets/Dialogue/DialogueScripts/RoomManager.cs
+++ RockinRacket/Assets/Dialogue/DialogueScripts/RoomManager.cs
@@ -2,9 +2,13 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.IO;
 
 public class RoomManager : MonoBehaviour
 {
+    private readonly string filePath = "Player/SaveFiles/";
+    private readonly string fileName = "Dialogue.json";
+
     [SerializeField] private List<GameObject> roomList; 
 
     public int currentHub = 1;
@@ -33,14 +37,15 @@ public class RoomManager : MonoBehaviour
     private bool hub4_Kurt_Interacted = false;
     private bool hub4_Haley_Interacted = false;
 
-    public static RoomManager instance;
+    private static RoomManager instance;
 
     void Awake()
     {
         if (instance == null)
         {
             instance = this;
-            StartingRoom();
+            instance.LoadDialogue();
+            instance.StartingRoom();
         }
         else
         {
@@ -55,7 +60,7 @@ public class RoomManager : MonoBehaviour
         {
             room.SetActive(false);
         }
-        GoToRoom("Ace Room");
+        GoToRoom("Garage");
     }
 
     public void GoToRoom(String roomName)
@@ -147,7 +152,7 @@ public class RoomManager : MonoBehaviour
         }
     }
 
-    public void setInteraction(int hub, string character)
+    public void SetInteraction(int hub, string character)
     {
         switch (hub)
         {
@@ -237,8 +242,132 @@ public class RoomManager : MonoBehaviour
         }
     }
 
-    public static RoomManager getInstance()
+    public static RoomManager GetInstance()
     {
         return instance;
+    }
+
+    //
+    // -- SAVING AND LOADING BELOW --
+    //
+
+    public void SaveDialogue()
+    {
+        Directory.CreateDirectory(filePath);
+
+        var dialogueData = new DialogueData
+        {
+            hub1_Ace_Interacted = hub1_Ace_Interacted,
+            hub1_MJ_Interacted = hub1_MJ_Interacted,
+            hub1_Kurt_Interacted = hub1_Kurt_Interacted,
+            hub1_Haley_Interacted = hub1_Haley_Interacted,
+
+            hub2_Ace_Interacted = hub2_Ace_Interacted,
+            hub2_MJ_Interacted = hub2_MJ_Interacted,
+            hub2_Kurt_Interacted = hub2_Kurt_Interacted,
+            hub2_Haley_Interacted = hub2_Haley_Interacted,
+
+            hub3_Ace_Interacted = hub3_Ace_Interacted,
+            hub3_MJ_Interacted = hub3_MJ_Interacted,
+            hub3_Kurt_Interacted = hub3_Kurt_Interacted,
+            hub3_Haley_Interacted = hub3_Haley_Interacted,
+
+            hub4_Ace_Interacted = hub4_Ace_Interacted,
+            hub4_MJ_Interacted = hub4_MJ_Interacted,
+            hub4_Kurt_Interacted = hub4_Kurt_Interacted,
+            hub4_Haley_Interacted = hub4_Haley_Interacted
+        };
+
+        string jsonData = JsonUtility.ToJson(dialogueData, prettyPrint: true);
+
+        File.WriteAllText(filePath + fileName, jsonData);
+    }
+
+    public void LoadDialogue()
+    {
+        string filePathAndName = filePath + fileName;
+        if (File.Exists(filePathAndName))
+        {
+            string jsonData = File.ReadAllText(filePath);
+            DialogueData loadedData = JsonUtility.FromJson<DialogueData>(jsonData);
+
+            hub1_Ace_Interacted = loadedData.hub1_Ace_Interacted;
+            hub1_MJ_Interacted = loadedData.hub1_MJ_Interacted;
+            hub1_Kurt_Interacted = loadedData.hub1_Kurt_Interacted;
+            hub1_Haley_Interacted = loadedData.hub1_Haley_Interacted;
+
+            hub2_Ace_Interacted = loadedData.hub2_Ace_Interacted;
+            hub2_MJ_Interacted = loadedData.hub2_MJ_Interacted;
+            hub2_Kurt_Interacted = loadedData.hub2_Kurt_Interacted;
+            hub2_Haley_Interacted = loadedData.hub2_Haley_Interacted;
+
+            hub3_Ace_Interacted = loadedData.hub3_Ace_Interacted;
+            hub3_MJ_Interacted = loadedData.hub3_MJ_Interacted;
+            hub3_Kurt_Interacted = loadedData.hub3_Kurt_Interacted;
+            hub3_Haley_Interacted = loadedData.hub3_Haley_Interacted;
+
+            hub4_Ace_Interacted = loadedData.hub4_Ace_Interacted;
+            hub4_MJ_Interacted = loadedData.hub4_MJ_Interacted;
+            hub4_Kurt_Interacted = loadedData.hub4_Kurt_Interacted;
+            hub4_Haley_Interacted = loadedData.hub4_Haley_Interacted;
+
+            Debug.Log("Dialogue successfully loaded");
+        }
+        else
+        {
+            ResetDialogue();
+            Debug.Log("No saved dialogue data found. Loading default dialogueData data.");
+        }
+    }
+
+    public void ResetDialogue()
+    {
+        hub1_Ace_Interacted = false;
+        hub1_MJ_Interacted = false;
+        hub1_Kurt_Interacted = false;
+        hub1_Haley_Interacted = false;
+
+        hub2_Ace_Interacted = false;
+        hub2_MJ_Interacted = false;
+        hub2_Kurt_Interacted = false;
+        hub2_Haley_Interacted = false;
+
+        hub3_Ace_Interacted = false;
+        hub3_MJ_Interacted = false;
+        hub3_Kurt_Interacted = false;
+        hub3_Haley_Interacted = false;
+
+        hub4_Ace_Interacted = false;
+        hub4_MJ_Interacted = false;
+        hub4_Kurt_Interacted = false;
+        hub4_Haley_Interacted = false;
+    }
+
+    // used to save conversations
+    private class DialogueData
+    {
+        // Hub 1 Dialogue Interacted Booleans
+        public bool hub1_Ace_Interacted = false;
+        public bool hub1_MJ_Interacted = false;
+        public bool hub1_Kurt_Interacted = false;
+        public bool hub1_Haley_Interacted = false;
+
+        // Hub 2 Dialogue Interacted Booleans
+        public bool hub2_Ace_Interacted = false;
+        public bool hub2_MJ_Interacted = false;
+        public bool hub2_Kurt_Interacted = false;
+        public bool hub2_Haley_Interacted = false;
+
+        // Hub 3 Dialogue Interacted Booleans
+        public bool hub3_Ace_Interacted = false;
+        public bool hub3_MJ_Interacted = false;
+        public bool hub3_Kurt_Interacted = false;
+        public bool hub3_Haley_Interacted = false;
+
+        // Hub 4 Dialogue Interacted Booleans
+        public bool hub4_Ace_Interacted = false;
+        public bool hub4_MJ_Interacted = false;
+        public bool hub4_Kurt_Interacted = false;
+        public bool hub4_Haley_Interacted = false;
     }
 }
