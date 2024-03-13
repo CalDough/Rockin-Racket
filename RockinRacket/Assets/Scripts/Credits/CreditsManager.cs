756944eb9613ad2435081d658dc6b0773d888993
Hamilton Gilbert <48567605+HamiltonGilbert@users.noreply.github.com>
2024-03-12 20:08:48 -0400

    worked on credits scripts

---
+++ RockinRacket/Assets/Scripts/Credits/CreditsManager.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class CreditsManager : MonoBehaviour
+{
+    [SerializeField] private CreditsPage[] creditsPages;
+    //[SerializeField] private GameObject nextBtn;
+    [SerializeField] private float animationTime;
+    private int currentPage = -1;
+    private bool isAnimating = false;
+
+    private void Start()
+    {
+        foreach (CreditsPage page in creditsPages)
+        {
+            page.FadeOut(0f);
+        }
+        StartCoroutine(NextPage());
+    }
+
+    public IEnumerator NextPage()
+    {
+
+        if (creditsPages.Length - 1 > currentPage && !isAnimating)
+        {
+            isAnimating = true;
+            // fade out old page
+            if (currentPage >= 0)
+            {
+                creditsPages[currentPage].FadeOut(animationTime);
+                yield return new WaitForSeconds(animationTime);
+            }
+            // fade in new page
+            currentPage++;
+            creditsPages[currentPage].FadeIn(animationTime);
+            yield return new WaitForSeconds(animationTime);
+            isAnimating = false;
+        }
+    }
+}
--- RockinRacket/Assets/Scripts/Credits/UIFade.cs
+++ RockinRacket/Assets/Scripts/Credits/UIFade.cs
@@ -3,19 +3,24 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
+[System.Serializable]
 public class UIFade : MonoBehaviour
 {
     [SerializeField] private MaskableGraphic UIElement;
-    public void Fade(float animationTime)
+    public void FadeIn(float animationTime)
     {
-        StartCoroutine(FadeElement(animationTime));
+        StartCoroutine(FadeElement(animationTime, new(UIElement.color.r, UIElement.color.g, UIElement.color.b, 1f)));
+    }
+    public void FadeOut(float animationTime)
+    {
+        StartCoroutine(FadeElement(animationTime, new(UIElement.color.r, UIElement.color.g, UIElement.color.b, 0f)));
     }
 
-    private IEnumerator FadeElement(float animationTime)
+    private IEnumerator FadeElement(float animationTime, Color endState)
     {
         float counter = 0f;
         Color startColor = UIElement.color;
-        Color endColor = new(UIElement.color.r, UIElement.color.g, UIElement.color.b, 0f);
+        Color endColor = endState;
         while (counter < animationTime)
         {
             counter += Time.unscaledDeltaTime;
