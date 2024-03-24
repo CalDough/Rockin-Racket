0530ed4473ad298ec470f5c3958ef42520f82ee4
xanderwilson030 <54119525+xanderwilson030@users.noreply.github.com>
2024-03-24 12:44:56 -0400

    Working on Intermission Alt Appearances

--- RockinRacket/Assets/Scripts/Concert Levels/Intermission/MerchTable/MTCustomer.cs
+++ RockinRacket/Assets/Scripts/Concert Levels/Intermission/MerchTable/MTCustomer.cs
@@ -12,12 +12,19 @@ public class MTCustomer : Attendee
     //private bool isInitialized = false;
     private bool markForDestruction = false;
 
+    public Sprite[] altAppearanceVariations;
+
     /*
      * This method is called by the Awake() method in the parent abstract class to ensure that the abstract class was intialized before the MTCustomer class
      */
     public override void Init()
     {
         //isInitialized = true;
+    }
+
+    private void Start()
+    {
+        RandomizeAppearanceAlt();
     }
 
     /*
@@ -58,5 +65,10 @@ public class MTCustomer : Attendee
     public void MarkCustomerForDestruction()
     {
         markForDestruction = true;
+    }
+
+    public void RandomizeAppearanceAlt()
+    {
+        sr.sprite = altAppearanceVariations[Random.Range(0, altAppearanceVariations.Length)];
     }
 }
